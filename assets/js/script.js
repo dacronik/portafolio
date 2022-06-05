83770d067b2619c5f95b06fa0d3de9fff1b84081
gerardo ortiz <dacronik@gmail.com>
2022-06-04 22:23:06 -0400

    primer commit

---
+++ assets/js/script.js
@@ -0,0 +1,74 @@
+
+
+//juego memorice
+let iconos = []
+    let selecciones = []
+
+    generarTablero()
+
+function cargarIconos() {
+    iconos = [
+        `<img src="./assets/img/1.svg">`,
+        `<img src="./assets/img/2.svg">`,
+        `<img src="./assets/img/3.svg">`,
+        `<img src="./assets/img/4.svg">`,
+        `<img src="./assets/img/5.svg">`,
+        `<img src="./assets/img/6.svg">`,
+        `<img src="./assets/img/7.svg">`,
+        `<img src="./assets/img/8.svg">`,
+    ]
+}
+
+function generarTablero() {
+    cargarIconos()
+    selecciones = []
+    let tablero = document.getElementById("tablero")
+    let tarjetas = []
+    for (let i = 0; i < 16; i++) {
+        tarjetas.push(`
+        <div class="area-tarjeta" onclick="seleccionarTarjeta(${i})">
+            <div class="tarjeta" id="tarjeta${i}">
+                <div class="cara trasera" id="trasera${i}">
+                    ${iconos[0]}
+                </div>
+                <div class="cara superior">
+                    <i class="fas fa-code"></i>
+                </div>
+            </div>
+        </div>        
+        `)
+        if (i % 2 == 1) {
+            iconos.splice(0, 1)
+        }
+    }
+    tarjetas.sort(() => Math.random() - 0.5)
+    tablero.innerHTML = tarjetas.join(" ")
+}
+
+function seleccionarTarjeta(i) {
+    let tarjeta = document.getElementById("tarjeta" + i)
+    if (tarjeta.style.transform != "rotateY(180deg)") {
+        tarjeta.style.transform = "rotateY(180deg)"
+        selecciones.push(i)
+    }
+    if (selecciones.length == 2) {
+        deseleccionar(selecciones)
+        selecciones = []
+    }
+}
+
+function deseleccionar(selecciones) {
+    setTimeout(() => {
+        let trasera1 = document.getElementById("trasera" + selecciones[0])
+        let trasera2 = document.getElementById("trasera" + selecciones[1])
+        if (trasera1.innerHTML != trasera2.innerHTML) {
+            let tarjeta1 = document.getElementById("tarjeta" + selecciones[0])
+            let tarjeta2 = document.getElementById("tarjeta" + selecciones[1])
+            tarjeta1.style.transform = "rotateY(0deg)"
+            tarjeta2.style.transform = "rotateY(0deg)"
+        }else{
+            trasera1.style.background = "plum"
+            trasera2.style.background = "plum"
+        }
+    }, 800);
+}
